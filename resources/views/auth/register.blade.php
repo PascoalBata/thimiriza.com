d470117be6e98892b1224898562e976b0f6b8431
PascoalBata <67436403+PascoalBata@users.noreply.github.com>
2020-08-11 17:17:40 +0200

    changes_110820v2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,68 +1,107 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container grey lighten-5" style="opacity: 80%; transform: translateY(2%);">
-    <div class="row">
-        <div class="col s12 m12 l12 center-align">
-            <div class="center-align">{{ __('Register') }}</div>
-        </div>
-    </div>
-    <div class="container row">
-        <div class="col s12 m12 l12">
-            <form method="POST" action="{{ route('register') }}" onsubmit="return submeter()">
-                @csrf
-                <div class="row">
-                    <div class="input-field col s12 m6 l6">
-                        <input id="name" name="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                        <label for="name" class="black-text">{{ __('Name') }}</label>
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="input-field col s12 m6 l6">
-                        <select id="type" name="type">
-                            <option value="" disabled selected>Tipo de Empresa</option>
-                            <option value="NORMAL">Normal</option>
-                            <option value="ISPC">ISPC</option>
-                        </select>
-                    </div>
+<div class="container grey lighten-5" style="opacity: 80%;">
+    <div class="card">
+        <div class="card-content">
+            <h4 class="center-align">{{ __('Registo de Empresa') }}</h4>
+            <div class="container row">
+                <div class="col s12 m12 l12">
+                    <form method="POST" action="{{ route('register') }}" onsubmit="return submeter()">
+                        @csrf
+                        <div class="row">
+                            <div class="input-field col s12 m6 l6">
+                                <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
+                                <label for="name" class="black-text">{{ __('Nome da empresa') }}</label>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="input-field col s12 m6 l6">
+                                <select id="type" name="type">
+                                    <optgroup label="{{__('Tipo')}}">
+                                        <option value="NORMAL">Normal</option>
+                                        <option value="ISPC">ISPC</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12 m6 l6">
+                                <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <label for="email" class="black-text">{{ __('E-Mail') }}</label>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="input-field col s12 m6 l6">
+                                <input id="phone" type="number" class="black-text validate" data-length="255" name="phone" value="{{ old('phone') }}" required>
+                                <label for="phone" class="black-text">{{ __('Telefone') }}</label>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12 m6 l6">
+                                <input type="number" class="black-text" id="nuit" name="nuit"
+                                       required value="{{old('nuit')}}">
+                                <label for="nuit" class="black-text">NUIT</label>
+                            </div>
+                            <div class="input-field col s12 m6 l6">
+                                <input type="text" class="black-text" id="empresa_endereco" name="empresa_endereco"
+                                       required value="{{old('empresa_endereco')}}">
+                                <label for="empresa_endereco" class="black-text">Endereço</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12 m6 l6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <label for="password" class="black-text">{{ __('Senha') }}</label>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="input-field col s12 m6 l6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <label for="password-confirm" class="black-text">{{ __('Confirme a senha') }}</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s12 m6 l6">
+                                <button type="submit" class="waves-effect waves-light btn-small">
+                                    {{ __('Registar') }}
+                                </button>
+                                <button type="reset" class="waves-effect waves-light btn-small">
+                                    {{ __('Limpar') }}
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            @if (Route::has('login'))
+                                <a href="{{ route('login') }}">
+                                    {{ __('Ja possui conta?') }}
+                                </a>
+                            @endif
+                        </div>
+                        <div>
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}">
+                                    {{ __('Esqueceu a senha?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </form>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12 m6 l6">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                        <label for="email" class="black-text">{{ __('E-Mail Address') }}</label>
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12 m6 l6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                        <label for="password" class="black-text">{{ __('Password') }}</label>
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="input-field col s12 m6 l6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                        <label for="password-confirm" class="black-text">{{ __('Confirm Password') }}</label>
-                    </div>
-                </div>
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Register') }}
-                        </button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
@@ -74,11 +113,6 @@
     });
     
     function submeter() {
-        if (!(document.getElementById('empresa_tipo').value === "ISPC" ||
-            document.getElementById('empresa_tipo').value === "NORMAL")) {
-                M.toast({html: 'Escolha o tipo de Empresa.', classes: 'rounded', displayLength: 1000});
-            return false;
-        }
         if(document.getElementById('senha_confirmacao').value !== document.getElementById('empresa_senha').value){
             M.toast({html: 'As senhas devem ser as mesmas.', classes: 'rounded', displayLength: 1000});
             return false;
