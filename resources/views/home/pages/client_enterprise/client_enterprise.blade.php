c5f1c9a29b07ec0a816dc3beb340caaaa5f54829
PascoalBata <67436403+PascoalBata@users.noreply.github.com>
2020-08-21 23:58:07 +0200

    210820_1158

--- resources/views/home/pages/client_enterprise/client_enterprise.blade.php
+++ resources/views/home/pages/client_enterprise/client_enterprise.blade.php
@@ -6,7 +6,7 @@
 <div class="container grey lighten-5" style="opacity: 80%; position: relative; transform: translateY(0%);">
     <div class="row center-align">
         <div class="col s12 m12 l12">
-            <h1 class="display-4 black-text"><strong>{{ __('Produtos') }}</strong></h1>
+            <h1 class="display-4 black-text"><strong>{{ __('Cliente Empresarial') }}</strong></h1>
         </div>
     </div>
     <div class="row" style="padding-bottom: 5%">
@@ -14,9 +14,9 @@
             @csrf
             <div class="row">
                 <div class="input-field col s12 m6 l6">
-                    <select id="type" name="type">
+                    <select id="type" name="type" onchange="changeClient(this.value)">
                         <optgroup label="{{__('Tipo')}}">
-                            <option value="ENTERPRISE">Empresarial</option>
+                            <option value="ENTERPRISE" selected>Empresarial</option>
                             <option value="SINGULAR">Singular</option>
                         </optgroup>
                     </select>
@@ -24,6 +24,26 @@
                 <div class="input-field col s12 m6 l6">
                     <label for="description" class="black-text">{{ __('Nome') }}</label>
                     <input id="description" type="text" class="black-text" name="description" value="{{ old('description') }}" required>
+                    @error('email')
+                        <span class="red-text" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 m6 l6">
+                    <label for="email" class="black-text">{{ __('Email') }}</label>
+                    <input id="email" type="email" class="black-text" name="email" value="{{ old('email') }}" required>
+                    @error('email')
+                        <span class="red-text" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="input-field col s12 m6 l6">
+                    <label for="price" class="black-text">{{ __('Preço') }}</label>
+                    <input id="price" type="text" class="black-text" name="price" value="{{ old('price') }}" required>
                     @error('email')
                         <span class="red-text" role="alert">
                             <strong>{{ $message }}</strong>
@@ -72,6 +92,20 @@
 </div>
 @endsection
 @section('script')
+<script>
+    function changeClient(value){
+        if(value === 'ENTERPRISE'){
+            window.history.replaceState(null, 'Thimiriza', '/clients_enterprise');
+            url = window.location.href;
+            window.location = url;
+            }
+        if(value === 'SINGULAR'){
+            url = window.history.replaceState(null, 'Thimiriza', '/clients_singular');
+            url = window.location.href;
+            window.location = url;
+        }
+    }
+</script>
     @if (session('product_register_status'))
     <div class="alert alert-success">
         <script>
--- resources/views/home/pages/client_singular/client_singular.blade.php
+++ resources/views/home/pages/client_singular/client_singular.blade.php
@@ -6,24 +6,23 @@
 <div class="container grey lighten-5" style="opacity: 80%; position: relative; transform: translateY(0%);">
     <div class="row center-align">
         <div class="col s12 m12 l12">
-            <h1 class="display-4 black-text"><strong>{{ __('Produtos') }}</strong></h1>
+            <h1 class="display-4 black-text"><strong>{{ __('Cliente Singular') }}</strong></h1>
         </div>
     </div>
     <div class="row" style="padding-bottom: 5%">
-        <form method="POST" action="{{ route('store_product') }}">
+        <form method="POST" action="{{ route('store_client_singular') }}">
             @csrf
             <div class="row">
                 <div class="input-field col s12 m6 l6">
-                    <label for="name" class="black-text">{{ __('Nome') }}</label>
-                    <input id="name" type="text" class="black-text" name="name" value="{{ old('name') }}" required>
-                    @error('email')
-                        <span class="red-text" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                    <select id="type" name="type" onchange="changeClient(this.value)">
+                        <optgroup label="{{__('Tipo')}}">
+                            <option value="ENTERPRISE">Empresarial</option>
+                            <option value="SINGULAR" selected>Singular</option>
+                        </optgroup>
+                    </select>
                 </div>
                 <div class="input-field col s12 m6 l6">
-                    <label for="description" class="black-text">{{ __('Descrição') }}</label>
+                    <label for="description" class="black-text">{{ __('Nome') }}</label>
                     <input id="description" type="text" class="black-text" name="description" value="{{ old('description') }}" required>
                     @error('email')
                         <span class="red-text" role="alert">
@@ -73,6 +72,20 @@
 </div>
 @endsection
 @section('script')
+<script>
+    function changeClient(value){
+        if(value === 'ENTERPRISE'){
+            window.history.replaceState(null, 'Thimiriza', '/clients_enterprise');
+            url = window.location.href;
+            window.location = url;
+            }
+        if(value === 'SINGULAR'){
+            url = window.history.replaceState(null, 'Thimiriza', '/clients_singular');
+            url = window.location.href;
+            window.location = url;
+        }
+    }
+</script>
     @if (session('product_register_status'))
     <div class="alert alert-success">
         <script>
